98328e25c230c89a6c1806930621d07727744a7d
GuProHub998 <98748268+Deemming998@users.noreply.github.com>
2022-01-31 21:45:35 +0700

    Create Ninja Legends.lua
    Ninja Script VIP

---
+++ Ninja Legends.lua
@@ -0,0 +1,17 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("GuPr0 Hub", "BloodTheme")
+
+local Tab = Window:NewTab("MENU")
+local Section = Tab:NewSection("Script Ninja Legend")
+Section:NewButton("Run ScriptNinjaLegend", "Run By. GuPr0 Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HydraVirgo/ninjalegendsfreegui/main/obfusc", true))()
+end)
+
+local Tab = Window:NewTab("INFO")
+local Section = Tab:NewSection("Info Team GuPr0")
+Section:NewButton("Info", "Make By. GuPr0 Hub 2022", function()
+end)
+Section:NewButton("GITHUB", "https://github.com/Deemming998 //GitHub", function()
+end)
+Section:NewButton("Pastebim", "https://pastebin.com/u/GuPr0Hub //Pastebin", function()
+end)
