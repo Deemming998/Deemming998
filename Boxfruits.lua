a66d77145dd5874d0b1c736a9a0ed6516c385444
GuProHub998 <98748268+Deemming998@users.noreply.github.com>
2022-01-31 19:40:45 +0700

    Update and rename ScriptTest1.lua to Boxfruits.lua

--- Boxfruits.lua
+++ Boxfruits.lua
@@ -1,13 +1,12 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("GuPr0 Hub", "BloodTheme")
- 
+
 local Tab = Window:NewTab("MENU")
 local Section = Tab:NewSection("Script Boxfuit")
-Section:NewButton("Run ScriptBoxfuit", "Make By. GuPr0 Hub", function()
+Section:NewButton("Run ScriptBoxfruits", "Run By. GuPr0 Hub", function()
     loadstring(game:HttpGet"https://raw.githubusercontent.com/natoppo044/modzcaster/main/Bfip.lua")()
 end)
-local Section = Tab:NewSection("Comming Soon!!")
- 
+
 local Tab = Window:NewTab("INFO")
 local Section = Tab:NewSection("Info Team GuPr0")
 Section:NewButton("Info", "Make By. GuPr0 Hub 2022", function()
